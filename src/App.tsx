8128e75924df34179531b98513e6c9e47b7e73e4
Cody Arnold <carnold@bluebeam.com>
2021-02-22 09:49:35 -0600

    Enforce chess logic and add Random Computer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Chessboard from "chessboardjsx";
-import { ChessInstance } from "chess.js";
+import { ChessInstance, ShortMove } from "chess.js";
 // chess types aren't playing nice right now
 const Chess = require("chess.js");
 
@@ -10,12 +10,37 @@ const App: React.FC = () => {
     new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
   );
 
+  const [fen, setFen] = useState(chess.fen());
+
+  const handleMove = (move: ShortMove) => {
+    if (chess.move(move)) {
+      setTimeout(() => {
+        const moves = chess.moves();
+
+        if (moves.length > 0) {
+          const computerMove = moves[Math.floor(Math.random() * moves.length)];
+          chess.move(computerMove);
+          setFen(chess.fen());
+        }
+      }, 300);
+
+      setFen(chess.fen());
+    }
+  };
+
   return (
     <div className="flex-center">
       <h1>Random Chess</h1>
       <Chessboard
         width={400}
-        position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        position={fen}
+        onDrop={(move) =>
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })
+        }
       />
     </div>
   );
